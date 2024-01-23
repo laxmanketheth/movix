1ed3c89fef7df75134f4da7b256920123089ea1c
Laxman <laxmanketheth76@gmail.com>
2024-01-23 20:29:27 +0800

    PageNotFound component added

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,9 @@ import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Details from "./pages/details/Details";
 import SearchResult from "./pages/searchResult/SearchResult";
-import Explore from "./pages/explore/explore";
+import Explore from "./pages/explore/Explore";
 import PageNotFound from "./pages/404/PageNotFound";
-import Home from './pages/home/home';
+import Home from './pages/home/Home';
 // import Home from "./pages/home/Home";
 
 
